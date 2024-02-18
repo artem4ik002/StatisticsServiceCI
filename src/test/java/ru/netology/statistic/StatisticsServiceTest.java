f999f91bd3c56a1ccdff3a9949d7ae8ddd5eedaf
Artem4ik <artem4iksmirnov1984gmail.com>
2024-02-18 15:33:31 +0300

    StatisticsServiceCI

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,23 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    public void findMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxIfB() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1};
+        long expected = 6;
 
         long actual = service.findMax(incomesInBillions);
 
